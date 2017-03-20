5def571f9e4ddfd0b4feb0aca48290a0174801c5
rezwan <rezwan.dev@gmail.com>
2017-03-20 18:18:21 +0600

    animation change

--- app/src/main/java/com/dhakariders/activity/LoginActivity.java
+++ app/src/main/java/com/dhakariders/activity/LoginActivity.java
@@ -1,10 +1,17 @@
 package com.dhakariders.activity;
 
+import android.animation.AnimatorSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
 
 import com.dhakariders.R;
 import com.dhakariders.utils.BlurView;
@@ -33,15 +40,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.createAccountButton:{
-                logInCardView.setVisibility(View.GONE);
-                signUpCardView.setVisibility(View.VISIBLE);
+                showSignUp();
                 break;
             }
             case R.id.cancelSignUp:{
-                signUpCardView.setVisibility(View.GONE);
-                logInCardView.setVisibility(View.VISIBLE);
+                closeSignUp();
                 break;
             }
         }
+    }
+
+    private void showSignUp() {
+        Animation side_in_bottom = AnimationUtils.loadAnimation(this,
+                R.anim.side_in_bottom);
+        Animation side_out_top = AnimationUtils.loadAnimation(this,
+                R.anim.side_out_top);
+        signUpCardView.setAnimation(side_in_bottom);
+        logInCardView.setAnimation(side_out_top);
+        signUpCardView.setVisibility(View.VISIBLE);
+        logInCardView.setVisibility(View.GONE);
+    }
+
+    private void closeSignUp(){
+        Animation side_in_top = AnimationUtils.loadAnimation(this,
+                R.anim.side_in_top);
+        Animation side_out_bottom = AnimationUtils.loadAnimation(this,
+                R.anim.side_out_bottom);
+        signUpCardView.setAnimation(side_out_bottom);
+        logInCardView.setAnimation(side_in_top);
+        logInCardView.setVisibility(View.VISIBLE);
+        signUpCardView.setVisibility(View.GONE);
+
     }
 }
