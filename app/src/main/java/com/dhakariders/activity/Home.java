5bdba664482d65113775f9b5ab25ed01ffbb0882
rezwan <rezwan.dev@gmail.com>
2017-03-27 15:07:51 +0600

    added maps

--- app/src/main/java/com/dhakariders/activity/Home.java
+++ app/src/main/java/com/dhakariders/activity/Home.java
@@ -1,13 +1,15 @@
 package com.dhakariders.activity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.dhakariders.R;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,18 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         ((TextView) findViewById(R.id.splashTextView))
                 .setTypeface(Typeface.createFromAsset(getAssets(), "fonts/splash_typeface.ttf"));
+        findViewById(R.id.hire).setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.hire:{
+                Intent intent  =  new Intent(this, PickUpAndDropOff.class);
+                startActivity(intent);
+                break;
+            }
+        }
     }
 }
